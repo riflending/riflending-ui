23f067804728f1a5fa03d92c3aaded8c04da24fc
facu <facundogregori@hotmail.com>
2020-12-04 19:00:50 -0300

    refact market to use util

--- src/middleware/market.js
+++ src/middleware/market.js
@@ -1,6 +1,8 @@
 import Rlending from '@riflending/riflending-js';
 import BigNumber from 'bignumber.js';
 import Util from './util'
+import { constants, decimals } from "./constants";
+import { ethers } from "ethers";
 
 
 /**
@@ -9,6 +11,9 @@ import Util from './util'
 export default class Market {
   // constructor() {}
   constructor(cTokenSymbol, cTokenDecimals, tokenSymbol, underlyingName, underlyingDecimals, account) {
+    //TODO see if util go to middleware class
+    this.util = new Util();
+    this.isCRBTC = (cTokenSymbol == 'cRBTC');
     let config = {
       1337: {
         httpProvider: 'http://127.0.0.1:8545',
@@ -20,21 +25,14 @@ export default class Market {
     // market.eventualWeb3WS = getEventualChainId().then((chainId) => new Rlending._ethers.providers.WebsocketProvider(config[chainId].wsProvider)).catch(() => new Error('Something went wrong with the web3 instance over web sockets on Market'));
     // market.eventualWeb3Http = new Rlending._ethers.providers.HttpProvider(config[32].httpProvider);
     this.decimals = cTokenDecimals;
-    this.instanceAddress = Rlending.util.getAddress(cTokenSymbol).toLowerCase();
-    this.instance = new Rlending._ethers.Contract(
-      Rlending.util.getAddress(cTokenSymbol),
-      ((cTokenSymbol) == 'cRBTC') ? Rlending.util.getAbi("CRBTC") : Rlending.util.getAbi("cErc20"),
-      Rlending.eth._createProvider(window.ethereum)
-    );
+    this.instanceAddress = this.util.addressContract[cTokenSymbol];
+    this.instance = this.util.getContractCtoken(cTokenSymbol);
+
     this.token = Object();
     //TODO
     //validate cRBTC
     if (cTokenSymbol != 'cRBTC') {
-      this.token.instace = new Rlending._ethers.Contract(
-        Rlending.util.getAddress(tokenSymbol),
-        Rlending.util.getAbi("cErc20"),
-        Rlending.eth._createProvider(window.ethereum)
-      );
+      this.token.instace = this.util.getContractCtoken(cTokenSymbol)
       this.token.internalAddress = Rlending.util.getAddress(tokenSymbol).toLowerCase();
     }
     //set data token
@@ -53,9 +51,6 @@ export default class Market {
     //TODO set supply of
     // https://github.com/ajlopez/DeFiProt/blob/master/contracts/Market.sol#L246
     this.supplyOf = 13;
-    //TODO
-    this.util = new Util();
-
   }
 
   async getValueMoc() {
@@ -72,89 +67,64 @@ export default class Market {
   }
 
   async getPrice() {
-    let priceToken;
+    //set contract
+    let contract = this.util.getContract('PriceOracleProxy');
     //get price of cToken
-    return Rlending.eth
-      .read(
-        Rlending.util.getAddress('PriceOracleProxy').toLowerCase(),
-        "function getUnderlyingPrice(address) returns (uint)",
-        [this.instanceAddress],
-        { provider: window.ethereum }
-      )
-      .then((priceResult) => {
-        priceToken = priceResult;
-        //get price of rbtc
-        return this.getValueMoc();
-      }).then((valueOracle) => {
-        //set decimals of token
-        let decimals = `1e${this.decimals}`;
-        // price = ( price cToken in rbtc * price of rbtc) / decimals of cToken
-        return new BigNumber(priceToken._hex).multipliedBy(new BigNumber(valueOracle)).div(new BigNumber(decimals)).toNumber();
-      });
+    let priceToken = await contract.getUnderlyingPrice(this.instanceAddress);
+    //get price of rbtc
+    let valueOracle = await this.getValueMoc();
+    //set decimals of cToken
+    let decimals = `1e${this.decimals}`;
+    // price = ( price cToken in rbtc * price of rbtc) / decimals of cToken
+    return new BigNumber(priceToken._hex).multipliedBy(new BigNumber(valueOracle)).div(new BigNumber(decimals)).toNumber();
   }
 
-  async getBalenceOfToken(acoount) {
-    return Rlending.eth
-      .read(
-        this.instanceAddress,
-        "function balanceOf(address) returns (uint)",
-        [acoount],
-        { provider: window.ethereum }
-      ).then((balance) => Rlending._ethers.utils.formatEther(balance));
+  async getBalenceOfToken(account) {
+    //set balance of account
+    let balance = await this.instance.balanceOf(account);
+    //return format (without wei)
+    return Rlending._ethers.utils.formatEther(balance);
   }
 
   async getCash() {
-    return Rlending.eth
-      .read(
-        this.instanceAddress,
-        "function getCash() returns (uint)",
-        [],
-        { provider: window.ethereum }
-      ).then((cash) => Number(cash));
+    //set balance of account
+    let cash = await this.instance.getCash();
+    return Number(cash);
   }
 
   async getBorrowRate() {
-    return Rlending.eth
-      .read(
-        this.instanceAddress,
-        "function borrowRatePerBlock() returns (uint)",
-        [],
-        { provider: window.ethereum }
-      )
-      .then((borrowRatePerBlock) => {
-        return new BigNumber(borrowRatePerBlock._hex).times(new BigNumber(100 * this.blocksPerYear)).div(new BigNumber(this.factor)).toNumber();
-      });
+    let borrowRatePerBlock = await this.instance.borrowRatePerBlock();
+    // return borrow rate
+    return new BigNumber(borrowRatePerBlock._hex).times(new BigNumber(100 * this.blocksPerYear)).div(new BigNumber(this.factor)).toNumber();
   }
 
-  validateMarketAccount(account, cTokenSymbol) {
-    //TODO add cache.
-    return Rlending.eth
-      .read(
-        Rlending.util.getAddress(Rlending.Unitroller),
-        "function checkMembership(address, address) returns (bool)",
-        [account, this.instanceAddress],
-        { provider: window.ethereum }
-      );
-    //"function getAssetsIn() returns (address[])",
+  async validateMarketAccount(account) {
+    //set contract Comptroller delegate (Unitroller)
+    let contract = this.util.getContractByNameAndAbiName(constants.Unitroller, constants.Comptroller);
+    //get is member (bool)
+    return await contract.checkMembership(account, this.instanceAddress);
   }
 
   async addMarkets() {
-    return Rlending.eth.trx(
-      Rlending.util.getAddress(Rlending.Unitroller),
-      "function enterMarkets(address[]) returns (bool)",
-      [[this.instanceAddress]],
-      { provider: window.ethereum }
-    );
+    //set contract
+    let contract = this.util.getContractByNameAndAbiName(constants.Unitroller, constants.Comptroller);
+    //set signer
+    let contractWithSigner = contract.connect(this.util.signer);
+    //send transaction
+    let tx = await contractWithSigner.enterMarkets([p.addressContract.cRIF]);
+    //await result transaction
+    return tx.wait();
   }
 
   async supply(amount, account) {
     //instace Rleanding js
     let instanceRlending = new Rlending(window.ethereum);
     //validate if market exist in account
-    return this.validateMarketAccount(account, this.token.symbol).then((ok) => {
+    return this.validateMarketAccount(account).then((ok) => {
       //if not exist => enterMarket
       //then supply
       if (!ok) {
+        //TODO remove this addMarkets and add it in supply button UI
         return this.addMarkets().then((enterMark) => {
           return instanceRlending.supply(this.token.symbol, amount);
         });
@@ -163,6 +133,30 @@ export default class Market {
     });
   }
 
+  //TODO this 
+  async supplyV2(amount, account) {
+    //add decimals token
+    amount = amount * Math.pow(10, decimals[this.token.symbol]);
+    amount = ethers.BigNumber.from(amount.toString());
+    const parameters = [];
+
+    if (!this.isCRBTC) {
+      //check allow
+      const allowance = await this.token.instace.allowance(account, this.instanceAddress);
+      const verify = allowance.lt(amount);
+      if (verify) {
+        //approve
+        let signer = this.token.instace.connect(this.util.signer);
+        await signer.approve(this.instanceAddress, amount);
+      }
+    }
+    if (cTokenName === constants.cRBTC) {
+      options.value = amount;
+    } else {
+      parameters.push(amount);
+    }
+
+  }
   /**
    * Borrows the specified amount from this market. May fail if no collateral has been supplied.
    * onto another market.
--- src/middleware/util.js
+++ src/middleware/util.js
@@ -5,15 +5,50 @@ export default class Util {
 
     constructor() {
         this.provider = new ethers.providers.Web3Provider(window.ethereum);
+        //add this format to support RSK
+        //validate formatt  provider
+        const format = this.provider.formatter.formats
+        if (format) format.receipt['root'] = format.receipt['logsBloom']
+        Object.assign(this.provider.formatter, { format: format });
         this.signer = this.provider.getSigner();
+        //asign format to signer
+        Object.assign(this.signer.provider.formatter, { format: format });
+
         this.addressContract = address.testnet;
     }
 
-    getContract(name) {
+    createContract(address, abi, provider) {
+        return new ethers.Contract(address, abi, provider);
+    }
+
+    validateContractName(name) {
         if (!constants.hasOwnProperty(name)) {
             console.error(`contract name (${name}) not exist in constants`);
-            return;
+            return false;
         }
-        return new ethers.Contract(this.addressContract[name], abi[name], this.provider);
+        return true;
+    }
+
+    getContract(name) {
+        if (this.validateContractName(name)) {
+            return this.createContract(this.addressContract[name], abi[name], this.provider);
+        }
+        return;
+    }
+
+
+    getContractCtoken(name) {
+        if (this.validateContractName(name)) {
+            let abiCtoken = ((name) == 'cRBTC') ? abi["cRBTC"] : abi["cErc20"];
+            return this.createContract(this.addressContract[name], abiCtoken, this.provider);
+        }
+        return;
+    }
+
+    getContractByNameAndAbiName(nameContract, nameAbi) {
+        if ((this.validateContractName(nameContract)) && (abi.hasOwnProperty(nameAbi))) {
+            return this.createContract(this.addressContract[nameContract], abi[nameAbi], this.provider);
+        }
+        return;
     }
 }
