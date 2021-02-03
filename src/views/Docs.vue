30f54b990aed6137aeeac97c0d9a302822ed8e0e
galimba <galimba@thinkanddev.com>
2021-02-03 10:47:45 -0300

    addedKeyconcept and HowTo

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -1,31 +1,52 @@
 <template>
-  <div class="admin">
-    <v-row class="d-flex justify-center">
-      <h1>Market Status</h1>
-    </v-row>
-    <div class="d-flex justify-center">
-      <v-card flat width="1250" class="list">
-        <v-row class="ma-0 d-flex justify-space-between">
-          <v-col class="d-flex align-center">
-            <h2>Listed Markets</h2>
+  <div class="docs">
+    <v-list>
+      <v-list-item>
+        <v-row>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">Introduction</v-list-item-subtitle>
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">Key Concepts</v-list-item-subtitle>
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">How To</v-list-item-subtitle>
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle" title="Loan to Value"
+              >Financial and Maths</v-list-item-subtitle
+            >
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">Smart Contracts</v-list-item-subtitle>
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">Oracles</v-list-item-subtitle>
+          </v-col>
+          <v-col>
+            <v-list-item-subtitle class="listTitle">Security and Audits</v-list-item-subtitle>
           </v-col>
         </v-row>
-        <Introduction />
-        <KeyConcepts />
-      </v-card>
-    </div>
+      </v-list-item>
+      <v-divider />
+      <Introduction />
+      <KeyConcepts />
+      <HowTo />
+    </v-list>
   </div>
 </template>
 
 <script>
 import Introduction from '@/components/docs/1-introduction.vue'
 import KeyConcepts from '@/components/docs/2-keyConcepts.vue'
+import HowTo from '@/components/docs/3-howTo.vue'
 
 export default {
   name: 'Docs',
   components: {
     Introduction,
     KeyConcepts,
+    HowTo,
   },
 }
 </script>
