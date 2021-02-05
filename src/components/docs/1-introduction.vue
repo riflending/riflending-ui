17cf469c1adcf91204a0cbf2750201fb91405c8e
Mariano Fauez <mariano.f@thinkanddev.com>
2021-02-05 18:38:08 -0300

    fix other docs view

--- src/components/docs/1-introduction.vue
+++ src/components/docs/1-introduction.vue
@@ -2,35 +2,39 @@
   <div>
     <v-container>
       <v-row class="d-flex align-center justify-Left">
-        <p>
-          <strong>rLending</strong> is a decentralized investment and finance (<em>DEFI</em>)
-          platform that takes advantage on <strong>Bitcoin</strong>&#39;s stability and the
-          <strong>RSK ecosystem</strong> to allow users to invest in a safe way.
-        </p>
-        <p>
-          <strong>rLenbding-protocol</strong> is based upon our own core of
-          <a href="https://github.com/riflending/rlending-protocol"><em>Smart Contracts</em></a>
-          which form an algorithmic distributed protocol deployed on the RSK network. This protocol
-          defines pools of assets with algorithmically derived interest rates, based on the supply
-          and demand for the assets. Suppliers (and borrowers) of an asset interact directly with
-          the protocol, earning (and paying) a floating interest rate, without having to negotiate
-          terms such as maturity, interest rate or collateral with a peer or counterparty.
-        </p>
-        <p>
-          Our main motivation is to allow users to lend crypto assets as collateral and to borrow
-          them based on interest rates set by real-time supply and demand.
-        </p>
-        <p>
-          In other words:
-          <strong
-            >Lenders earn revenue in exchange for their supplied assets and borrowers pay interests
-            for the assets they used.</strong
-          >
-        </p>
-        <p>
-          In this pages we&#39;ll guide you through some of the basic features you will most likely
-          be interested in using.
-        </p>
+        <v-col cols="12">
+          <h1>Introduction:</h1>
+          <p>
+            <strong>rLending</strong> is a decentralized investment and finance (<em>DEFI</em>)
+            platform that takes advantage on <strong>Bitcoin</strong>&#39;s stability and the
+            <strong>RSK ecosystem</strong> to allow users to invest in a safe way.
+          </p>
+          <p>
+            <strong>rLenbding-protocol</strong> is based upon our own core of
+            <a href="https://github.com/riflending/rlending-protocol"><em>Smart Contracts</em></a>
+            which form an algorithmic distributed protocol deployed on the RSK network. This
+            protocol defines pools of assets with algorithmically derived interest rates, based on
+            the supply and demand for the assets. Suppliers (and borrowers) of an asset interact
+            directly with the protocol, earning (and paying) a floating interest rate, without
+            having to negotiate terms such as maturity, interest rate or collateral with a peer or
+            counterparty.
+          </p>
+          <p>
+            Our main motivation is to allow users to lend crypto assets as collateral and to borrow
+            them based on interest rates set by real-time supply and demand.
+          </p>
+          <p>
+            In other words:
+            <strong
+              >Lenders earn revenue in exchange for their supplied assets and borrowers pay
+              interests for the assets they used.</strong
+            >
+          </p>
+          <p>
+            In this pages we&#39;ll guide you through some of the basic features you will most
+            likely be interested in using.
+          </p>
+        </v-col>
       </v-row>
     </v-container>
   </div>
