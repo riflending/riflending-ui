840b27af2632b88eaae0537fa187743d0503ef65
galimba <galimba@thinkanddev.com>
2021-02-09 10:16:05 -0300

    adding general questions

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -3,7 +3,8 @@
     <v-container>
       <v-row class="d-flex align-center justify-left">
         <v-col cols="12">
-          <h1 id="whatIs">What exactly is rLending?</h1>
+          <center><h1 id="faq">FAQs</h1></center>
+          <h2 id="whatIs">What exactly is rLending?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -11,7 +12,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="howWorks">How does it work?</h1>
+          <h2 id="howWorks">How does it work?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -19,7 +20,31 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="howConnect">How do I connect?</h1>
+          <h2 id="howCost">How much does it cost? Can I try it for free?</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <h2 id="howProfit">How does rLending make money?</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <h2 id="testnet">How do I connect to the Test Network?</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <h2 id="mainnet">How do I connect to the main net?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -38,7 +63,7 @@
             This will prompt you to choose your account within <em>Metamask/Nifty</em>. After you
             have accepted the connection, you will be redirected to the <em>Home Dashboard</em>.
           </p>
-          <h1 id="gasPrice">Why is gas price so high?</h1>
+          <h2 id="gasPrice">Why is gas price so high?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -46,7 +71,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="canTrade">Is rLending a Distributed Exchange (DEX)?</h1>
+          <h2 id="canTrade">Is rLending a Distributed Exchange (DEX)?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -54,7 +79,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="earnings">How do users earn money with rLending?</h1>
+          <h2 id="earnings">How do users earn money with rLending?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -62,7 +87,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="howMuchEarnings">How much earnings can I expect?</h1>
+          <h2 id="howMuchEarnings">How much earnings can I expect?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -70,7 +95,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="howBorrow">How does borrowing work?</h1>
+          <h2 id="howBorrow">How does borrowing work?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -78,7 +103,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="whatFees">What are the fees for borrowing?</h1>
+          <h2 id="whatFees">What are the fees for borrowing?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -86,7 +111,7 @@
             <strong>Connect Wallet</strong>. The screen will let you connect your
             <em>Metamask/Nifty</em> wallet.
           </p>
-          <h1 id="crossBorrow">Can I borrow from a market I have not supplied to?</h1>
+          <h2 id="crossBorrow">Can I borrow from a market I have not supplied to?</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
             connected to the <strong>RSK network</strong> Direct your browser towards the
@@ -95,7 +120,33 @@
             <em>Metamask/Nifty</em> wallet.
           </p>
           <hr />
-          <h1 id="troubleshooting">Troubleshooting:</h1>
+          <center><h1 id="dev">I am a developer</h1></center>
+          <h2 id="contracts">How do I interact with the smart contracts?</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <h2 id="docs">Where can I find more info on...</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <h2 id="howToDev">How can I contribute?</h2>
+          <p>
+            In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
+            connected to the <strong>RSK network</strong> Direct your browser towards the
+            <a href="www.rlending.app"><strong>rLending</strong> website</a> and click
+            <strong>Connect Wallet</strong>. The screen will let you connect your
+            <em>Metamask/Nifty</em> wallet.
+          </p>
+          <hr />
+          <center><h1 id="troubleshooting">Troubleshooting</h1></center>
           <h2>I can't connect:</h2>
           <p>
             In order to use <strong>rLending</strong> you will need your <em>Metamask</em> wallet
