92da15f30a5aa98bbcdeb839385e80e5ecc9d082
galimba <galimba@thinkanddev.com>
2021-02-17 10:58:42 -0300

    adding vue-list-items instead of <p>

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -3,39 +3,138 @@
     <div>
       <v-container>
         <v-navigation-drawer absolute permanent expand-on-hover>
-          <p>FAQs</p>
-          <p><a href="#whatIs">What exactly is rLending?</a></p>
-          <p><a href="#canTrade">is rLending a DEX?</a></p>
-          <p><a href="#howWorks">How does rLending work?</a></p>
-          <p><a href="#howCost">Can I try it for free?</a></p>
-          <p><a href="#getWallet">How do I get a wallet?</a></p>
-          <p><a href="#howProfit">How does rLending profit?</a></p>
-          <p><a href="#wallet">How to install a wallet</a></p>
-          <p><a href="#testnet">Connecting to Testnet</a></p>
-          <p><a href="#mainnet">Connecting to Mainnet</a></p>
-          <p><a href="#gasPrice">Why Is Gas Price So High?</a></p>
-          <p><a href="#earnings">How do accounts earn interests?</a></p>
-          <p><a href="#howMuchEarnings">How much earnings can I expect?</a></p>
-          <p><a href="#canBorrow">Can I borrow without a deposit? </a></p>
-          <p><a href="#howBorrow">How does Borrowing work? </a></p>
-          <p>
-            <a href="#borrowFee">What fees are involved in Borrowing? </a>
-          </p>
-          <p><a href="#howColFact">How does Collateral Factor work?</a></p>
-          <p><a href="#crossBorrow">Can I borrow from a market I have not supplied to?</a></p>
-          <p><a href="#whatDebt">What happens if someone doesn't pay their debt?</a></p>
-          <p>
-            <a href="#reserves"
-              >What's preventing someone from withdrawing all of the balance in the contracts?</a
+          <v-list-item>
+            <v-list-item-content
+              ><p>FAQs</p>
+              <v-list-item-subtitle> Frequently Asked Questions</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <a href="#whatIs">What exactly is rLending?</a></v-list-item-content
             >
-          </p>
-          <p><a href="#priceVariation">How Can I Have More Debt Than Before? </a></p>
-          <p><a href="#healthFactor">What Is A Health Factor? </a></p>
-          <p><a href="#liquidation">What Happens If My Health Factor Becomes Negative?</a></p>
-          <p><a href="#security">How Secure Is rLending? </a></p>
-          <p><a href="#dev">I am a Developer </a></p>
-          <p><a href="#help">General Troubleshooting </a></p>
-          <p><a href="#support">How do I contact Support?</a></p>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#canTrade">is rLending a DEX?</a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#howWorks">How does rLending work?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#howCost">Can I try it for free?</a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#howProfit">How does rLending profit?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#getWallet">How do I get a wallet?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#wallet">How to install a wallet</a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#testnet">Connecting to Testnet</a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#mainnet">Connecting to Mainnet</a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#gasPrice">Why Is Gas Price So High?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#earnings">How do accounts earn interests?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#howMuchEarnings">How much earnings can I expect?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#canBorrow">Can I borrow without a deposit? </a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#howBorrow">How does Borrowing work? </a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <a href="#borrowFee">What fees are involved in Borrowing? </a>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#howColFact">How does Collateral Factor work?</a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#crossBorrow"
+                >Can I borrow from a market I have not supplied to?</a
+              ></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#whatDebt"
+                >What happens if someone doesn't pay their debt?</a
+              ></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <a href="#reserves"
+                >What's preventing someone from withdrawing all of the balance in the contracts?</a
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#priceVariation"
+                >How Can I Have More Debt Than Before?
+              </a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#healthFactor">What Is A Health Factor? </a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#liquidation"
+                >What Happens If My Health Factor Becomes Negative?</a
+              ></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#security">How Secure Is rLending? </a></v-list-item-content
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#dev">I am a Developer </a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content><a href="#help">General Troubleshooting </a></v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content
+              ><a href="#support">How do I contact Support?</a></v-list-item-content
+            >
+          </v-list-item>
         </v-navigation-drawer>
       </v-container>
     </div>
