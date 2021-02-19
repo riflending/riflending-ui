835547cc7fe38f90a1abc33e17bb93e6f7616b1c
pedro <pedro@thinkanddev.com>
2021-02-19 09:28:06 -0300

    Add mainnet deploy

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,16 @@
-const { gitDescribeSync } = require('git-describe');
-const { version } = require('./package.json');
+const { gitDescribeSync } = require('git-describe')
+const { version } = require('./package.json')
 
 process.env.VUE_APP_GIT_HASH = gitDescribeSync({
-  customArguments: ['--abbrev=40'] // full 40-character SHA-1 commit hash
+  customArguments: ['--abbrev=40'], // full 40-character SHA-1 commit hash
 }).hash
 
 module.exports = {
-  transpileDependencies: [
-    'vuetify',
-    'VueGoogleCharts',
-  ],
+  transpileDependencies: ['vuetify', 'VueGoogleCharts'],
   lintOnSave: false,
   configureWebpack: {
     output: {
       filename: `[name]-${version}.[hash].bundle.js`,
-    }
-  }
-};
+    },
+  },
+}
