c631de7cfba2457110d3c1db1d324a0a2fb7dd71
galimba <galimba@thinkanddev.com>
2021-02-24 13:03:01 -0300

    Update src/views/Terms.vue
    Co-authored-by: Pedrow <pmprete@users.noreply.github.com>

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -275,7 +275,7 @@
             and that although the Interface may be available in other jurisdictions, its
             availability does not give rise to general or specific personal jurisdiction in any
             forum outside the State of Delaware. Any arbitration conducted pursuant to this
-            Agreement shall be governed by the Federal Arbitration Act. You agree that San
+            Agreement shall be governed by the Federal Arbitration Act. You agree that
             Wilmington, Delaware is the proper forum for any appeals of an arbitration award or for
             court proceedings in the event that this Agreement’s binding arbitration clause is found
             to be unenforceable.
