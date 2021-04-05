ba90cc109f89819de78f31cb7758834f7da20af6
facu <facundogregori@hotmail.com>
2021-04-05 15:29:37 -0300

    remove log and home path in Landing page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,6 @@ import Landing from '@/views/Landing.vue'
 import store from '@/store'
 
 const requireAuth = (to, from, next) => {
-  console.log('SESSION_IS_LOGGED', store.getters.SESSION_IS_LOGGED)
   if (!store.getters.SESSION_IS_LOGGED) {
     next({ name: 'Landing' })
   } else {
@@ -12,7 +11,7 @@ const requireAuth = (to, from, next) => {
 
 export default [
   {
-    path: '/home',
+    path: '/',
     name: 'Landing',
     component: Landing,
   },
